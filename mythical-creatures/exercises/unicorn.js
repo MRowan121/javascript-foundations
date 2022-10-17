626549d022a5ab1c609d087b8b5c88b6013246f2
Matt Rowan <matt.rowan121@gmail.com>
2022-10-17 17:48:02 -0400

    feat: Return false if color is not white

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,7 +2,14 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = (color || "white");
-  }
-}
+  };
+
+  isWhite() {
+    if(this.color !== "white") {
+      return false;
+    };
+  };
+
+};
 
 module.exports = Unicorn;
