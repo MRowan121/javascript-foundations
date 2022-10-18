dab90da693d0c5e1243fb1f32faa3690c8dc8651
Matt Rowan <matt.rowan121@gmail.com>
2022-10-17 20:12:11 -0400

    feat: Make ounces drank dynamic

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,12 +1,13 @@
 class Vampire {
-    constructor(name, pet) {
+    constructor(name, pet, ouncesDrank) {
         this.name = name;
         this.pet = (pet || "bat");
         this.thirsty = true;
-        this.ouncesDrank = 0;
+        this.ouncesDrank = (ouncesDrank || 0);
     };
 
     drink() {
+        this.ouncesDrank = this.ouncesDrank + 10;
         return this.thirsty = false;
     };
 
