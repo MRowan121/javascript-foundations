d426dd1211dbf9c5c2683e744a6360dc310b1853
Matt Rowan <matt.rowan121@gmail.com>
2022-10-18 09:13:51 -0400

    Remove all skips

--- mythical-creatures/test/magician-test.js
+++ mythical-creatures/test/magician-test.js
@@ -4,12 +4,12 @@ var Magician = require('../exercises/magician');
 describe('Magician', function() {
 // Oh no! It looks like there's no text in the `it` blocks! I guess you'll have to read the tests SUPER carefully to know what the test is expecting!
 
-it.skip('', function() {
+it('should be a function', function() {
     assert.isFunction(Magician);
     assert.instanceOf(new Magician({}), Magician);
   });
 
-it.skip('', function() {
+it('should have a name', function() {
     var magician1 = new Magician({ name: 'Scott' });
     var magician2 = new Magician({ name: 'Casey' });
 
@@ -17,7 +17,7 @@ it.skip('', function() {
     assert.equal(magician2.name, 'The Great Casey');
   });
 
-it.skip('', function() {
+it('should be able to have an assistant', function() {
     var magician1 = new Magician({ name: 'Scott', assistant: true });
     var magician2 = new Magician({ name: 'Casey', assistant: false });
 
@@ -28,19 +28,19 @@ it.skip('', function() {
     assert.equal(magician2.assistant, false);
   });
 
-it.skip('', function() {
+it('should have a top hat as favorite accesory if name is Scott', function() {
     var magician = new Magician({ name: 'Scott' });
 
     assert.equal(magician.favoriteAccessory, 'top hat');
   });
 
-it.skip('', function() {
+it('should have a cape as favorite accesory if name is Casey', function() {
     var magician = new Magician({ name: 'Casey', clothing: 'cape' });
 
     assert.equal(magician.favoriteAccessory, 'cape')
   });
 
-it.skip('', function() {
+it('should capitalize spells and charms with a function', function() {
     var magician = new Magician({ name: 'Scott' });
 
     var spell = magician.performIncantation('abracadabra');
@@ -50,7 +50,7 @@ it.skip('', function() {
     assert.equal(charm, 'ALLAKAZAAM!');
   });
 
-it.skip('', function() {
+it('should perform trick', function() {
     var magician1 = new Magician({ name: 'Hannah' });
     var magician2 = new Magician({ name: 'Kayla', clothing: 'top hat' });
 
@@ -61,7 +61,7 @@ it.skip('', function() {
     assert.equal(spell2, 'PULL RABBIT FROM TOP HAT');
   });
 
-it.skip('', function() {
+it('should perform different trick if clothing is cape', function() {
     var magician = new Magician({ name: 'Leta', clothing: 'cape' });
 
     var spell = magician.performTrick();
@@ -69,13 +69,13 @@ it.skip('', function() {
     assert.equal(spell, 'PULL DOVE FROM SLEEVE');
   });
 
-it.skip('', function() {
+it('should establish confidence percentage at 10', function() {
     var magician = new Magician({ name: 'Leta' });
 
     assert.equal(magician.confidencePercentage, 10);
   });
 
-it.skip('', function() {
+it('should increase confidence percentage by 10 with each trick', function() {
     var magician = new Magician({ name: 'Leta' });
 
     magician.performTrick();
@@ -89,7 +89,7 @@ it.skip('', function() {
     assert.equal(magician.confidencePercentage, 50);
   });
 
-it.skip('', function() {
+it('should be able to saw someone in half if confidence is at 100%', function() {
     var magician = new Magician({ name: 'Scott', assistant: true });
 
     assert.equal(magician.performShowStopper(), 'Oh no, this trick is not ready!');
@@ -101,7 +101,7 @@ it.skip('', function() {
     assert.equal(magician.performShowStopper(), 'WOW! The magician totally just sawed that person in half!');
   });
 
-it.skip('', function() {
+it('should not be able to saw someone in half without an assistant', function() {
     var magician1 = new Magician({ name: 'Scott', assistant: false });
     var magician2 = new Magician({ name: 'Hannah', assistant: true });
 
