5b87fe8634643eee86e7626021140a2809d0242b
Matt Rowan <matt.rowan121@gmail.com>
2022-10-17 22:29:51 -0400

    feat: Add celebrate birthday function

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,8 +1,12 @@
 class Hobbit {
-    constructor(name) {
+    constructor(name, age) {
         this.name = name.name;
-        this.age = 0;
-    }
+        this.age = (age || 0);
+    };
+
+    celebrateBirthday() {
+        this.age = this.age + 1;
+    };
 };
 
 module.exports = Hobbit;
