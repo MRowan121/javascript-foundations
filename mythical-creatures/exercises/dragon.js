b409383f48646388004c2e0956435591113c3461
Matt Rowan <matt.rowan121@gmail.com>
2022-10-17 20:01:29 -0400

    feat: Let the dragon eat

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,12 +1,20 @@
 class Dragon {
-    constructor(name, rider) {
+    constructor(name, rider, mealsEaten) {
         this.name = name;
         this.rider = rider;
         this.hungry = true;
+        this.mealsEaten = (mealsEaten || 0);
     };
 
     greet() {
         return `Hi, ${this.rider}!`;
+    };
+
+    eat() {
+        this.mealsEaten = this.mealsEaten + 1;
+        if(this.mealsEaten >= 3) {
+            return this.hungry = false;
+        };
     };
 
 };
