8fef7ae1ca7e29aaa4c7f7a124f87e8d493e3c47
Matt Rowan <matt.rowan121@gmail.com>
2022-10-17 22:32:21 -0400

    feat: Make the hobbit a child

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -2,11 +2,13 @@ class Hobbit {
     constructor(name, age) {
         this.name = name.name;
         this.age = (age || 0);
+        this.adult = false;
     };
 
     celebrateBirthday() {
         this.age = this.age + 1;
     };
+
 };
 
 module.exports = Hobbit;
