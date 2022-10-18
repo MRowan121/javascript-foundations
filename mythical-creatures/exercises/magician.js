60e01c01391129ff2061ed2a5f8aaf16ebbc609f
Matt Rowan <matt.rowan121@gmail.com>
2022-10-17 23:46:20 -0400

    feat: Add incantation function

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -1,9 +1,14 @@
 class Magician {
-    constructor(name, clothing) {
+    constructor(name) {
         this.name = `The Great ` + name.name;
         this.assistant = name.assistant
         this.favoriteAccessory = (name.clothing || "top hat");
     };
+
+    performIncantation(magic) {
+    return magic.toUpperCase() + `!`
+    };
+
 };
 
 
