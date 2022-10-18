ad94b1c1c308e6738602706fbdaee50cabd297a2
Matt Rowan <matt.rowan121@gmail.com>
2022-10-17 22:41:53 -0400

    feat: Make the hobbit grow old

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -3,12 +3,15 @@ class Hobbit {
         this.name = name.name;
         this.age = (age || 0);
         this.adult = false;
+        this.old = false;
     };
 
     celebrateBirthday() {
         this.age = this.age + 1;
-        if(this.age >= 33) {
+        if(this.age >= 33 && this.age <= 100) {
             this.adult = true;
+        } else if (this.age >= 101) {
+            this.old = true;
         };
     };
 
