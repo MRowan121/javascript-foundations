725eb4f86d73661abc1e15231c06b5835c577bdf
Matt Rowan <matt.rowan121@gmail.com>
2022-10-17 21:08:53 -0400

    feat: Add transform werewolf function

--- mythical-creatures/exercises/werewolf.js
+++ mythical-creatures/exercises/werewolf.js
@@ -2,7 +2,13 @@ class Werewolf {
     constructor(name) {
         this.name = name;
         this.form = "human";
-    }
+    };
+
+    completeTransformation() {
+        this.form = "wolf";
+        return `Aaa-Woooo!`;
+    };
+
 };
 
 module.exports = Werewolf;
