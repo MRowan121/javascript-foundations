d45c817da04f04b5ca180e81ce622739b09a3467
Matt Rowan <matt.rowan121@gmail.com>
2022-10-18 09:04:39 -0400

    feat: Increase confidence percentage with each trick

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -11,6 +11,7 @@ class Magician {
     };
 
     performTrick() {
+        this.confidencePercentage = this.confidencePercentage + 10;
         if(this.favoriteAccessory === "cape") {
             return `PULL DOVE FROM SLEEVE`
         } else {
