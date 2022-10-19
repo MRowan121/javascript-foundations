27459dc8fa6932f93a4c12ac0c03ef14d65efbbd
Matt Rowan <matt.rowan121@gmail.com>
2022-10-19 13:30:07 -0400

    feat: Change freed persons mood

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -14,6 +14,7 @@ class Medusa {
         } else if (this.statues.length === 3) {
             this.statues.push(statue);
             var human = new Person(this.statues.shift().name);
+            human.mood = "relieved";
             return human;
         };
     };
