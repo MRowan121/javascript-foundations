3ad621a0a4437ff952e8775ab81c12d1031af577
Matt Rowan <matt.rowan121@gmail.com>
2022-10-19 10:29:24 -0400

    feat: Add gaze at victim function

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -5,9 +5,15 @@ class Medusa {
     constructor(name1) {
         this.name = name1;
         this.statues = [];
-    }
+    };
+
+    gazeAtVictim(victim) {
+        var statue = new Statue(victim.name)
+        this.statues.push(statue);
+        console.log(this.statues)
+    };
 };
 
 
 
-module.exports = Medusa;
+module.exports = Medusa; Person; Statue;
