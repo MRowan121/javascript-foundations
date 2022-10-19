f314a65b9687fdf2ab5b6ee80655d8bd5763a886
Matt Rowan <matt.rowan121@gmail.com>
2022-10-18 22:44:25 -0400

    feat: Crerate person class

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -2,10 +2,12 @@ var Person = require('./person');
 var Statue = require('./statue');
 
 class Medusa {
-    constructor(name) {
-        this.name = name;
+    constructor(name1) {
+        this.name = name1;
         this.statues = [];
     }
 };
+
+
 
 module.exports = Medusa;
