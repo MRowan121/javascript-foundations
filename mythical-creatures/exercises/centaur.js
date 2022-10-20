bac83fc6b9981dbd8ab48ab30ae71675f024ab88
Matt Rowan <matt.rowan121@gmail.com>
2022-10-20 10:24:44 -0400

    feat: Make centuar cranky after exercising

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -1,17 +1,30 @@
 class Centaur {
-    constructor(name) {
+    constructor(name, exercise) {
         this.name = name.name;
         this.breed = name.type;
         this.cranky = false;
         this.standing = true;
+        this.exercise = (exercise || 0);
     };
 
     shootBow() {
+        this.exercise = this.exercise + 1;
+        if(this.exercise === 3) {
+            this.cranky = true;
+            return 'Twang!!!';
+        } else {
         return 'Twang!!!'
+        };
     };
 
     run() {
-        return 'Clop clop clop clop!!!'
+        this.exercise = this.exercise + 1;
+        if(this.exercise === 3) {
+            this.cranky = true;
+            return 'Clop clop clop clop!!!';
+        } else {
+            return 'Clop clop clop clop!!!';
+        };
     };
 };
 
