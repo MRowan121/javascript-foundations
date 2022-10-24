0778dd2fbdf09f9ebbd32654d23b362cc0bbe9f7
Matt Rowan <matt.rowan121@gmail.com>
2022-10-24 10:12:35 -0400

    feat: Add remaining functions

--- mythical-creatures/exercises/fairy.js
+++ mythical-creatures/exercises/fairy.js
@@ -1,8 +1,10 @@
 class Fairy {
-    constructor(fairy) {
+    constructor(fairy, disposition) {
         this.name = fairy;
         this.dust = 10;
         this.clothes = {dresses:['Iris']};
+        this.disposition = (disposition || 'Good natured')
+        this.humanWards = [];
     };
 
     receiveBelief() {
@@ -17,6 +19,19 @@ class Fairy {
         this.clothes.dresses.push(...flower)
     };
 
+    becomeProvoked() {
+        this.disposition = 'Vengeful';
+    };
+
+    replaceInfant(infant) {
+        if(this.disposition === 'Vengeful' && this.humanWards.length < 2) {
+            infant.disposition = 'Malicious';
+            this.humanWards.push(infant);
+        } else {
+            this.disposition = 'Good natured'
+            return infant;
+        }
+    };
 };
 
 module.exports = Fairy;
