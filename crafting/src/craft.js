918aeb053c05dc6fe3838408181eebb3652fab68
Matt Rowan <matt.rowan121@gmail.com>
2022-10-24 15:01:49 -0400

    Add some functions

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,9 @@
+var Material = require('./material')
+
 class Craft {
+    constructor(craft) {
+        this.name = craft.type;
+    };
+};
 
-}
-
-module.exports = Craft;
+module.exports = Craft; Material;
