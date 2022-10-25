2345fdbaa27e982dcab313d9ecf4c7d1ae4f6a1d
Matt Rowan <matt.rowan121@gmail.com>
2022-10-25 09:56:05 -0400

    Add remaining functions

--- mythical-creatures/exercises/ogre.js
+++ mythical-creatures/exercises/ogre.js
@@ -2,10 +2,23 @@ class Ogre {
     constructor(name) {
         this.name = name.name;
         this.home = (name.abode || 'Swamp');
+        this.swings = (this.swings || 0);
     };
 
     encounter(human) {
         human.encounterCounter += 1;
+        if(human.encounterCounter > 0 && human.encounterCounter % 3 === 0) {
+            this.swings += 1
+            human.knockedOut = true;
+        };
+    };
+
+    swingAt() {
+        this.swings += 1;
+    };
+
+    apologize(human) {
+        human.knockedOut = false;
     };
 
 };
