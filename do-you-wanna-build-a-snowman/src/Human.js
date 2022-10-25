ab93202cf92810328a042940319dfa3b68609093
Matt Rowan <matt.rowan121@gmail.com>
2022-10-24 20:56:47 -0400

    Add human class with functions

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,34 @@
 var Snowman = require('./Snowman');
 
 class Human {
+    constructor(name) {
+        this.name = name;
+        this.wantsToBuildASnowman = true;
+        this.materials = {
+            snowballs: 0,
+            coal: 0,
+            buttons: 0,
+            carrots: 0,
+        };
+    };
 
-}
+    gatherMaterials(materialName, materialCount) {
+        this.materials[materialName] += materialCount;
+    };
 
-module.exports = Human;
+    buildASnowman() {
+        var snowman = new Snowman(this.materials);
+        return snowman;
+    };
+
+    placeMagicHat(snowman) {
+        if (snowman.canWearMagicHat()) {
+            return `Woah, this snowman is coming to life!`
+        } else {
+        return `I guess I didn't build it correctly.`
+    };
+};
+
+};
+
+module.exports = Human; Snowman;
