601b514257d86e34ad5c49596f954ad48ba4a59e
Matt Rowan <matt.rowan121@gmail.com>
2022-10-25 15:02:28 -0400

    Add more functions

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -11,9 +11,14 @@ class Sphinx {
             this.riddles.push(riddle)
         }
     };
-
-    attemptAnswer() {
-        // maybe use find index to find which riddle contains the provided answer?
+    
+    attemptAnswer(answer) {
+        for(var i = 0; i < this.riddles.length; i++) {
+            if(this.riddles[i].answer === answer) {
+                this.riddles.splice([i], 1)
+                return `That wasn't that hard, I bet you don't get the next one`
+            };
+        };
     };
 };
 
