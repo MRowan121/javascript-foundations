9d8c0203ee3ba7591a792b158c9c080ab788ecf9
Matt Rowan <matt.rowan121@gmail.com>
2022-10-26 16:10:58 -0400

    Add remaining functions

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -3,7 +3,23 @@ var Material = require('./material')
 class Craft {
     constructor(craft) {
         this.name = craft.type;
+        this.materials = craft.materials;
+        this.completed = false;
     };
+
+    completeCraft() {
+        this.completed = true;
+        return `All done! It looks great!`;
+    };
+
+    calculateProjectTotal() {
+        var totalCost = 0;
+        for(var i = 0; i < this.materials.length; i++) {
+            totalCost += parseInt(this.materials[i].calculateMaterialCost())
+        };
+        return totalCost;
+    };
+
 };
 
 module.exports = Craft; Material;
