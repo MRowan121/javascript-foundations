08747d23a00dba6fec2564c53e707aecd1b82754
Matt Rowan <matt.rowan121@gmail.com>
2022-10-27 11:09:38 -0400

    Add some functions

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,25 @@
+const { Runner } = require("mocha");
+
 class Roadrace {
+    constructor(name) {
+        this.name = name.title;
+        this.location = name.city;
+        this.distance = 0;
+        this.participants = [];
+    };
 
-}
+    setDistance(totalDistance) {
+        this.distance = totalDistance;
+        return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+    };
 
-module.exports = Roadrace;
+    registerParticipants(runner) {
+        this.participants.push(runner);
+    };
+
+    completeRace() {
+        Runner.runRace(this.name, this.distance);
+    };
+};
+
+module.exports = Roadrace; Runner;
