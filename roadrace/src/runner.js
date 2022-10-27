7a123de8562b269799c878460fbecad82250a886
Matt Rowan <matt.rowan121@gmail.com>
2022-10-27 10:11:34 -0400

    feat: Add runner class and functions

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,27 @@
 class Runner {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.milesRun = (this.milesRun || 0);
+        this.fitness = (this.fitness || 25);
+        this.completedRaces = [];
+    };
 
-}
+    runSomeMiles(milesRan) {
+        this.milesRun += milesRan;
+        this.fitness += milesRan;
+    };
+
+    stretch() {
+        this.fitness += .5;
+    };
+
+    runRace(raceName, milesRan) {
+        this.milesRun += milesRan;
+        this.fitness += milesRan;
+        this.completedRaces.push(raceName);
+    };
+
+};
 
 module.exports = Runner;
