2fab8ab97c4471299b8db06e539710699e631177
Matt Rowan <matt.rowan121@gmail.com>
2022-10-30 21:07:20 -0400

    Add golf course class

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,5 +1,10 @@
 class GolfCourse {
-
+    constructor(name, difficulty, openings, [feature1, feature2]) {
+        this.name = name;
+        this.difficulty = difficulty;
+        this.openings = openings;
+        this.features = [feature1, feature2];
+    }
 
 }
 
