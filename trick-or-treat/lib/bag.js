f737401e0764a97676b0947374fce618b235e826
Matt Rowan <matt.rowan121@gmail.com>
2022-10-30 21:45:41 -0400

    Add bag class

---
+++ trick-or-treat/lib/bag.js
@@ -0,0 +1,25 @@
+var Candy = require('./candy')
+
+class Bag {
+    constructor() {
+        this.empty = true;
+        this.count = 0;
+        this.candies = [];
+    };
+
+    fill(candy) {
+        this.candies.push(candy);
+        this.count = this.count + 1;
+    };
+
+    contains(candy) {
+        for(var i = 0; i < this.candies.length; i++) {
+            if(this.candies[i].includes(candy)) {
+                return true;
+            }
+        }
+    }
+
+};
+
+module.exports = Bag;
