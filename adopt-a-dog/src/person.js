3fb087d5cebed15978c630628e6a6c3af48e1dda
Matt Rowan <matt.rowan121@gmail.com>
2022-11-03 15:04:01 -0400

    Add person class and functions

--- adopt-a-dog/src/person.js
+++ adopt-a-dog/src/person.js
@@ -1,10 +1,32 @@
 var Dog = require("./dog.js");
 
 class Person {
-  constructor() {
+  constructor(name, dog) {
+    this.name = name;
+    this.dog = dog;
+  };
 
+  fillFoodBowl() {
+    this.dog.eat()
+  };
+
+  throwBall() {
+    this.dog.fetchBall();
+    return `${this.dog.name} loves playing fetch!`
   }
 
-}
+  introduceNewFriends(dog) {
+    this.dog.makeNewFriend(dog);
+  }
+
+  adoptAPup(name, age) {
+    if(this.dog !== undefined) {
+      return `You can't adopt ${name}. You already have ${this.dog.name}!`
+    } else {
+    this.dog = new Dog({name: name, age: age})
+  };
+};
+
+};
 
 module.exports = Person;
