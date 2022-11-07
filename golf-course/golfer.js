32bdd1ca85cd77d23b13eb1acc0751047b8c6966
Matt Rowan <matt.rowan121@gmail.com>
2022-11-06 19:22:23 -0500

    Build golfer class

--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,5 +1,29 @@
-class Golfer {
+const GolfCourse = require("./golf-course");
 
+class Golfer {
+    constructor(golferDetails) {
+        this.name = golferDetails.name;
+        this.handicap = golferDetails.handicap;
+        this.frustration = 0;
+        this.confidence = 0;
+    };
+
+    calculateAvg(par) {
+        this.handicap = this.handicap + par;
+        return `I usually shoot a ${this.handicap} on average.`
+    };
+
+    playRound(GolfCourse) {
+        if(GolfCourse.difficulty === 'moderate') {
+            this.frustration = 100;
+        } else {
+            this.frustration = 500;
+        }
+    };
+
+    simulatePractice(num) {
+        this.confidence = this.confidence + (10 * num)
+    };
 
 }
 
