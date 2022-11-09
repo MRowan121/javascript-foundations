c0f3a611d3ecac1f2b794b9e3fb52dd3c4068369
Matt Rowan <matt.rowan121@gmail.com>
2022-11-09 10:21:02 -0500

    Add snack class

--- vending-machine/src/snack.js
+++ vending-machine/src/snack.js
@@ -1,5 +1,22 @@
 class Snack {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+        this.itemsInStock = 0;
+    };
 
-}
+    stockItems(newInv) {
+        this.itemsInStock = this.itemsInStock + newInv
+    };
+
+    removeItem() {
+        if(this.itemsInStock === 0) {
+            return `Sorry, no ${this.name} left in stock!`
+        } else {
+            this.itemsInStock = this.itemsInStock - 1;
+            return `Item taken! There are now ${this.itemsInStock} items left.`
+        }
+    };
+};
 
 module.exports = Snack;
