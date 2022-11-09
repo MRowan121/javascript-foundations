5c8e4bbeb8634615c16d2e2c15b27637eeb214d4
Matt Rowan <matt.rowan121@gmail.com>
2022-11-09 11:08:55 -0500

    Begin class buildout

--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,27 @@
 class VendingMachine {
+    constructor(machineDetails) {
+        this.id = machineDetails.id;
+        this.isBroken = machineDetails.isBroken;
+        this.snacks = [];
+    };
 
-}
+    addSnacks(snackType) {
+        for(var i = 0; i < this.snacks.length; i++) {
+            if(this.snacks[i].name === snackType.name) {
+                return `Sorry, that snack is already stocked! Try adding a different snack.`
+            }
+        }
+        this.snacks.push(snackType);
+    };
+
+    purchaseSnack(snackName, payment) {
+        for(var i = 0; i < this.snacks.length; i++) {
+            if(this.snacks[i].name === snackName) {
+                this.snacks[i].removeItem()
+                return `Success! Here is $${payment - this.snacks[i].price} back!`
+            }
+        };
+    }
+};
 
 module.exports = VendingMachine;
