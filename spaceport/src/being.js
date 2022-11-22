c15fb8efdf01de6000838c17bfa983fdffa51ec7
Matt Rowan <matt.rowan121@gmail.com>
2022-11-22 16:32:54 -0500

    Add being class

--- spaceport/src/being.js
+++ spaceport/src/being.js
@@ -1,4 +1,14 @@
 class Being {
+    constructor(name, species) {
+        this.isAlive = true;
+        this.name = name;
+        this.species = species;
+        this.credits = 0;
+    };
+
+    updateCredits(num) {
+        this.credits = this.credits + num;
+    };
 }
 
 module.exports = Being;
